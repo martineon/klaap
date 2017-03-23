43fa5abd3f050020f1f6f8bc8d92027ae53c9dbc
nitramnoe <eon.martin@icloud.com>
2017-03-23 15:28:46 +0100

    ajout input

--- src/landingPage/landingPage.jsx
+++ src/landingPage/landingPage.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './landingPage.css';
+import logoLanding from '../../public/assets/logo.svg'
 
 
 class LandingPage extends Component {
   render() {
     return (
       <div className="container-fullpage">
-
+        <div className="logo-landing-box">
+          <img src={logoLanding} className="logo-landing" alt="Logo"/>
+        </div>
         <div className="input-container">
+          <h1 className="text-form-landing">Laissez-nous votre email pour savoir quand la plateforme sera ouverte</h1>
           <form method="post" action="/api/users">
-            <input type="text" name="email" placeholder="EMAIL"/>
-            <input type="text" name="name" placeholder="NAME"/>
-            <button type="submit">Submit</button>
+            <div className="input-box">
+              <input type="text" className="email-input" autocomplete="off" name="email" placeholder="email"/>
+              <input type="text" className="name-input" autocomplete="off" name="name" placeholder="prenom"/>
+              <button type="submit" className="button-submit-landing">valider</button>
+            </div>
           </form>
         </div>
       </div>
